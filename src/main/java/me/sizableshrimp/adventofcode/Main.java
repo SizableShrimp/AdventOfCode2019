3d374cc74986643149566d5c89e9a4a975395ed0
SizableShrimp <29855886+SizableShrimp@users.noreply.github.com>
2019-12-06 22:43:31 -0600

    Update Main and fix empty line inconsistencies.

--- src/main/java/me/sizableshrimp/adventofcode/Main.java
+++ src/main/java/me/sizableshrimp/adventofcode/Main.java
@@ -7,27 +7,19 @@ package me.sizableshrimp.adventofcode;
 
 import me.sizableshrimp.adventofcode.helper.DataReader;
 import me.sizableshrimp.adventofcode.templates.Day;
-import org.reflections.Reflections;
 
-import java.lang.reflect.Modifier;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.time.ZoneId;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class Main {
-    private static Map<String, Class<? extends Day>> days =
-            new Reflections(Main.class.getPackageName()).getSubTypesOf(Day.class).stream()
-                    .filter(c -> !Modifier.isAbstract(c.getModifiers()))
-                    .collect(Collectors.toMap(Class::getSimpleName, Function.identity()));
     /**
      * The hardcoded Advent Of Code year.
      * This year integer is only used for retrieving data from the AOC servers with {@link DataReader#read}.
      * Otherwise, it is useless.
      */
     public static final int YEAR = 2019;
+    private static final String BASE_PACKAGE = Main.class.getPackageName() + ".days.";
 
     public static void main(String[] args) {
         run();
@@ -45,12 +37,12 @@ public class Main {
     }
 
     private static void run(int dayOfMonth) {
-        Class<? extends Day> clazz = days.get("Day" + pad(dayOfMonth));
-        if (clazz == null)
-            return;
-        System.out.println("Day " + dayOfMonth + ":");
         try {
-            clazz.getDeclaredConstructor().newInstance().run();
+            Class<?> clazz = Class.forName(BASE_PACKAGE + "Day" + pad(dayOfMonth));
+            if (clazz == null)
+                return;
+            System.out.println("Day " + dayOfMonth + ":");
+            ((Day) clazz.getDeclaredConstructor().newInstance()).run();
         } catch (Exception e) {
             e.printStackTrace();
         }
