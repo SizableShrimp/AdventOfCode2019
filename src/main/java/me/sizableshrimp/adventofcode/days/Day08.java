42bcd87d312930d26b1c814efe01033eac8ea322
SizableShrimp <29855886+SizableShrimp@users.noreply.github.com>
2019-12-08 06:24:37 -0600

    Complete Day 8.

---
+++ src/main/java/me/sizableshrimp/adventofcode/days/Day08.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2019 SizableShrimp.
+ * This file is provided under version 3 of the GNU Lesser General Public License.
+ */
+
+package me.sizableshrimp.adventofcode.days;
+
+import me.sizableshrimp.adventofcode.helper.GridHelper;
+import me.sizableshrimp.adventofcode.templates.SeparatedDay;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day08 extends SeparatedDay {
+    List<int[][]> layers = new ArrayList<>();
+    int width = 25;
+    int height = 6;
+
+    @Override
+    protected Object part1() {
+        String line = lines.get(0);
+        int size = width * height;
+        for (int i = 0; i < line.length(); i += size) {
+            int[][] grid = new int[height][width];
+            String layer = line.substring(i, i + size);
+            fillGrid(layer, grid);
+            layers.add(grid);
+        }
+        return layers.stream()
+                .min(Comparator.comparing(g -> GridHelper.countOccurrences(g, 0)))
+                .map(g -> GridHelper.countOccurrences(g, 1) * GridHelper.countOccurrences(g, 2)).get();
+    }
+
+    @Override
+    protected Object part2() {
+        StringBuilder sb = new StringBuilder("\n");
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                List<Integer> values = getAtPosition(i, j);
+                addPosition(sb, values);
+            }
+            sb.append('\n');
+        }
+        return sb;
+    }
+
+    private void addPosition(StringBuilder sb, List<Integer> values) {
+        int result = -1;
+        for (int i = 1; i < values.size(); i += 2) {
+            int front = values.get(i - 1);
+            int behind = values.get(i);
+            result = front == 2 ? behind : front;
+            if (result != 2)
+                break;
+        }
+        sb.append(result == 1 ? "##" : "  "); //in my opinion, it is much more readable when doubled up
+    }
+
+    private void fillGrid(String layer, int[][] grid) {
+        for (int i = 0; i < layer.length(); i++) {
+            int row = i / width;
+            int column = i % width;
+            grid[row][column] = Integer.parseInt(layer.substring(i, i + 1));
+        }
+    }
+
+    private List<Integer> getAtPosition(int i, int j) {
+        return layers.stream()
+                .map(g -> g[i][j])
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/me/sizableshrimp/adventofcode/helper/GridHelper.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2019 SizableShrimp.
+ * This file is provided under version 3 of the GNU Lesser General Public License.
+ */
+
+package me.sizableshrimp.adventofcode.helper;
+
+public class GridHelper {
+    public static <T> int countOccurrences(T[][] grid, T target) {
+        int result = 0;
+        for (T[] row : grid) {
+            for (T t : row) {
+                if (target.equals(t))
+                    result++;
+            }
+        }
+        return result;
+    }
+
+    public static int countOccurrences(int[][] grid, int target) {
+        int result = 0;
+        for (int[] row : grid) {
+            for (int i : row) {
+                if (i == target)
+                    result++;
+            }
+        }
+        return result;
+    }
+}
